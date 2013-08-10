f20b47fc659ae36103b95ffb5eddf29e34baad3f
Mickey <codemickeycode@gmail.com>
2013-08-10 20:20:59 +0800

    fix username

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def check_exists_by_id(e_id):
         return False
     return element
 
-uname = settings.credentials['uname']
+uname = settings.credentials['username']
 pwd = settings.credentials['pwd']
 
 # Go to the login page
@@ -25,7 +25,7 @@ browser.get("http://www.pizzahut.com.ph/2007/order.php#") # Load page
 check_elem_id = check_exists_by_id("frm_email")
 if check_elem_id:
     element = check_elem_id
-    element.send_keys(name)
+    element.send_keys(uname)
     time.sleep(0.2) # Let the page load
 
 check_elem_id = check_exists_by_id("frm_pass1")
